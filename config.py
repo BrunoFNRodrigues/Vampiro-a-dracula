2c82f03b82ae413de7388b32aece0d86a23920cd
BrunoFNRodrigues <17669@gmail.com>
2020-05-13 15:40:50 -0300

    Criando o arquivo de configurações

---
+++ config.py
@@ -0,0 +1,12 @@
+from os import path
+
+# Define a pasta das imagens e audios.
+IMG_DIR = path.join(path.dirname(__file__), 'assets', 'img')
+SOM_DIR = path.join(path.dirname(__file__), 'assets', 'som')
+
+# Dados de tela.
+LARGURA = 1100
+ALTURA = 720
+FPS = 60 # Taxa de atualização em frames por segundo
+
+# Tamanho dos elementos
