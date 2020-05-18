ad58fe582717d5d1cf4c7db1e52b36cf5c0e34be
BrunoFNRodrigues <17669@gmail.com>
2020-05-18 17:36:24 -0300

    Trilha sonora no assets

--- assets.py
+++ assets.py
@@ -6,8 +6,13 @@ BACKGROUND = 'background'
 HEROI_IMG = 'heroi_img'
 DRACULA_IMG =   'dracula_img'
 
+
 def load_assets():
     assets = {}
     assets[BACKGROUND] = pygame.image.load(os.path.join(IMG_DIR, 'cthulhu.jpg')).convert()
 
-    
+    #Carrega os sons do jogo
+    pygame.mixer.music.load(os.path.join(SND_DIR, 'Tank!'))
+    pygame.mixer.music.set_volume(0.4)
+
+    return assets
