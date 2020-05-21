213b424d4a1d7ac249e65dfeaf42b8d0c6908418
Bruno Rodrigues <bf17669@gmail.com>
2020-05-21 17:19:10 -0300

    implementação das opções de combate

--- battle_screen.py
+++ battle_screen.py
@@ -1,6 +1,7 @@
 #-------INCIANDO---------
 #Importa e incia as biblietecas
 import pygame
+import random
 from config import ALTURA, LARGURA, FPS
 from assets import load_assets, BACKGROUND
 
@@ -24,14 +25,16 @@ def battle_screen(window):
         state = PLAYING
 
         keys_down = {}
-        health = 100
+        hero_health = 100
+        boss_health = 300
+        guard = True
 
         #======Ciclo principal=======
         pygame.mixer.music.play(loops=-1)
         while state != DONE:
             clock.tick(FPS)
 
-            #Trate eventos
+            #Trata eventos
             for event in pygame.event.get():
                 #Verifica consequências
                 if event.type == pygame.QUIT:
@@ -40,4 +43,20 @@ def battle_screen(window):
                 if state == PLAYING:
                     #Verifica se apertou alguma tecla.
                     if event.type == pygame.KEYDOWN:
+                        #Seleciona opções no menu de combate
+                        keys_down[event.key] = True
+                        #Define a função de ataque
+                        if event.key == pygame.K_1:
+                            damage = 10+random.randint(1,15)
+                            boss_health -= damage
+                        #Define a ação de defender
+                        if event.key == pygame.K_2:
+                            guard = True
+                        #Define a ação de usar cura
+                        if event.key == pygame.K_3:
+                            if hero_health<100:
+                                hero_health += 5+random.randint(0,10)
+                        #Define a ação de fugir
+                        if event.key == pygame.k_4:
+                            state = DONE
                         
