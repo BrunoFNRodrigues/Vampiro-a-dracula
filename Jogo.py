b658f8d0d1dca5b874e6a9eb9350fec25b601b7c
BrunoFNRodrigues <17669@gmail.com>
2020-05-21 20:04:01 -0300

    Criação do arquivo executor do jogo

---
+++ Jogo.py
@@ -0,0 +1,23 @@
+#===inciando===
+#Importa e inicia pacotes
+import  pygame
+import random
+from config import *
+from battle_screen import battle_screen
+
+pygame.init()
+pygame.mixer.init()
+
+#=====>Gera tela de combate
+window = pygame.display.set_mode((LARGURA, ALTURA))
+pygame.display.set_caption('Vampiro a Dracula')
+
+state = INIT
+while state != QUIT:
+    if state == BATTLE:
+        state = battle_screen(window)
+    else:
+        state = QUIT
+
+#_______ENCERRANDO________
+pygame.quit() #Função que finaliza o pygame
