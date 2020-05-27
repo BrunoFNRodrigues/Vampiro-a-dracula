059b7757c3b48d70006f4f21f1f8199ab93b7c6c
BrunoFNRodrigues <17669@gmail.com>
2020-05-27 10:43:09 -0300

    Atualização dos sprites(Vida e dano)

--- sprites.py
+++ sprites.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from config import *
 from assets import *
 
@@ -13,6 +14,12 @@ class Hero(pygame.sprite.Sprite):
         self.rect.left = LARGURA / 4
         self.rect.top = 20
         self.groups = groups
+        self.assets = assets
+        self.health = 100
+    def attack(self):
+        #Gera o dano do ataque 
+        self.damage = 20 #ERRO AO DEFINIR DANO ALEATÓRIO
+        self.assets[DAMAGING_SOUND].play()
 
 class Boss(pygame.sprite.Sprite):
     def __init__(self, assets):
@@ -23,4 +30,4 @@ class Boss(pygame.sprite.Sprite):
        self.rect = self.image.get_rect()
        self.rect.right = LARGURA*3 / 4
        self.rect.top = 20
-       
+       self.health = 200
