0a646203d5d90e2aefa0d21531cdfa8b57e909cf
PauloAugusto815 <pauloapo@al.insper.edu.br>
2020-06-03 15:38:04 -0300

    nova movimentação

--- assets.py
+++ assets.py
@@ -8,7 +8,7 @@ DRACULA_IMG = 'dracula_img'
 SCORE_FONT = 'score_font'
 DYING_SOUND = 'dying_sound'
 DAMAGING_SOUND = 'damaging_sound'
-
+HEROI2_IMG = 'heroi2_img'
 
 
 def load_assets():
@@ -20,7 +20,8 @@ def load_assets():
     assets[DRACULA_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'Dracula.jpg')).convert_alpha()
     assets[DRACULA_IMG] = pygame.transform.scale(assets['dracula_img'], (DRACULA_LARGURA, DRACULA_ALTURA))
     assets[SCORE_FONT] = pygame.font.Font(os.path.join(FNT_DIR, 'PressStart2P.ttf'), 28)
-    
+    assets[HEROI2_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'Heroi2.jpg')).convert_alpha()
+    assets[HEROI2_IMG] = pygame.transform.scale(assets['heroi2_img'], (HEROI_LARGURA, HEROI_ALTURA))
     #Carrega os sons do jogo
     pygame.mixer.music.load(os.path.join(SOM_DIR, 'tgfcoder-FrozenJam-SeamlessLoop.ogg'))
     pygame.mixer.music.set_volume(0.4)
--- sprites.py
+++ sprites.py
@@ -8,19 +8,27 @@ class Hero(pygame.sprite.Sprite):
     def __init__(self, groups, assets):
         # Construindo o sprite
         pygame.sprite.Sprite.__init__(self)
-
-        self.image = assets[HEROI_IMG]
-        self.rect = self.image.get_rect()
-        self.rect.centerx = LARGURA / 4
-        self.rect.top = 20
+        self.image = 0
+        self.lutando = False
         self.groups = groups
         self.assets = assets
         self.health = 100
         self.velocidade_x=0
         self.velocidade_y=0
     def update(self):
+        if self.lutando:
+            self.image = self.assets[HEROI_IMG]
+            self.rect = self.image.get_rect()
+            self.rect.centerx = LARGURA / 4
+            self.rect.top = 20
+        else:
+            self.image = self.assets[HEROI2_IMG]
+            self.rect = self.image.get_rect()
+            self.rect.centerx = LARGURA/2
+            self.rect.centery = ALTURA/2
         # Atualização da posição do jogador
         self.rect.x += self.velocidade_x
+        self.rect.y += self.velocidade_y
         # Mantem dentro da tela
         if self.rect.right > LARGURA:
             self.rect.right = LARGURA
